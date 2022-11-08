c61f643dd92e6c9c89559181b8a113ff53cde4e0
Ilya Danilov <esozap32@mail.ru>
2022-11-08 23:33:13 +0400

    TriangleCycle

---
+++ src/main/java/TriangleCycle.java
@@ -0,0 +1,16 @@
+public class TriangleCycle {
+    public static void main(String[] args) {
+
+
+        for (int i = 0; i < 10; i++) {
+            int starCount = i;
+            for (int j = -1; j < starCount; j++) {
+                System.out.print("3");
+            }
+
+            System.out.println();
+        }
+    }
+}
+
+
